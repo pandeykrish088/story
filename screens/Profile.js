f873c400d608fe621c12c02921452492953053f8
pandeykrish088 <76039564+pandeykrish088@users.noreply.github.com>
2021-10-31 14:20:12 +0530

    Add files via upload

---
+++ screens/Profile.js
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react';
+import {  View, Text } from 'react-native';
+
+class Profile extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> Profile </Text>
+      </View>
+    );
+  }
+}
+
+export default Profile;
